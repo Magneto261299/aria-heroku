ce6448425dabe009fc881ad72af61a3ec6ac4f5a
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-11-09 23:47:51 +0530

    Update reg_exps.ts

--- src/bot_utils/reg_exps.ts
+++ src/bot_utils/reg_exps.ts
@@ -1,23 +1,23 @@
 export class RegExps {
   readonly start: RegExp;
-  readonly mirrorTar: RegExp;
-  readonly mirror: RegExp;
-  readonly mirrorStatus: RegExp;
-  readonly list: RegExp;
-  readonly getFolder: RegExp;
-  readonly cancelMirror: RegExp;
-  readonly cancelAll: RegExp;
+  readonly zip: RegExp;
+  readonly darpan: RegExp;
+  readonly sthiti: RegExp;
+  readonly suchi: RegExp;
+  readonly folder: RegExp;
+  readonly kill: RegExp;
+  readonly killall: RegExp;
   readonly disk: RegExp;
 
   constructor(commands: string[]) {
     this.start = new RegExp(commands[0], 'i');
-    this.mirrorTar = new RegExp(commands[1], 'i');
-    this.mirror = new RegExp(commands[2], 'i');
-    this.mirrorStatus = new RegExp(commands[3], 'i');
-    this.list = new RegExp(commands[4], 'i');
-    this.getFolder = new RegExp(commands[5], 'i');
-    this.cancelMirror = new RegExp(commands[6], 'i');
-    this.cancelAll = new RegExp(commands[7], 'i');
+    this.zip = new RegExp(commands[1], 'i');
+    this.darpan = new RegExp(commands[2], 'i');
+    this.sthiti = new RegExp(commands[3], 'i');
+    this.suchi = new RegExp(commands[4], 'i');
+    this.folder = new RegExp(commands[5], 'i');
+    this.kill = new RegExp(commands[6], 'i');
+    this.killall = new RegExp(commands[7], 'i');
     this.disk = new RegExp(commands[8], 'i');
   }
 }
