c78c36e422c9fcbfc5c6efba1579e6496ea6c778
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-11-09 23:45:54 +0530

    Update event_regex.ts

--- src/bot_utils/event_regex.ts
+++ src/bot_utils/event_regex.ts
@@ -7,13 +7,13 @@ export class EventRegex {
   readonly commandsRegexNoName: regexps.RegExps;
 
   constructor() {
-    var commands = ['^/start', '^/mirrorTar', '^/mirror', '^/mirrorStatus', '^/list', '^/getFolder', '^/cancelMirror', '^/cancelAll', '^/disk'];
+    var commands = ['^/start', '^/zip', '^/darpan', '^/sthiti', '^/suchi', '^/folder', '^/kill', '^/killall', '^/disk'];
     var commandsNoName: string[] = [];
-    var commandAfter = ['$', ' (.+)', ' (.+)', '$', ' (.+)', '$', '$', '$', '$'];
+    var commandAfter = ['$', ' (.+)', ' (.+)', '$', ' (.+)', '$', '$', '$'];
 
     if (constants.COMMANDS_USE_BOT_NAME && constants.COMMANDS_USE_BOT_NAME.ENABLED) {
       commands.forEach((command, i) => {
-        if (command === '^/list') {
+        if (command === '^/suchi') {
           // In case of more than one of these bots in the same group, we want all of them to respond to /list
           commands[i] = command + commandAfter[i];
         } else {
